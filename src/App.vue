966a697fb8905c5469c38b544a1510cf2de852d2
Lex <sevic.official@gmail.com>
2020-07-22 18:20:10 +0200

    Styling navigation

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+}
+</style>
--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,10 +1,23 @@
 <template>
-  <nav class="row">
-    <h4 class="column"><strong>PORTFOLIO</strong></h4>
-    <h5 class="column">About me</h5>
-    <h5 class="column">Projects</h5>
-    <h5 class="column">More</h5>
-  </nav>
+  <header class="nav">
+    <h2 class="nav__logo">PORTFOLIO</h2>
+    <nav>
+      <ul class="nav__links">
+        <li>
+          <a href="#">About me</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">More</a>
+        </li>
+      </ul>
+    </nav>
+    <a href="#" class="cta">
+      <button>Contact me</button>
+    </a>
+  </header>
 </template>
 
 <script>
@@ -13,4 +26,65 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+li,
+a,
+button {
+  font-size: 18px;
+  color: #edf0f1;
+  text-decoration: none;
+}
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #201a1a;
+  padding: 30px 10%;
+}
+
+.nav__logo {
+  cursor: pointer;
+  color: #f0f0f0;
+  -webkit-user-select: none; /* Safari */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* IE10+/Edge */
+  user-select: none; /* Standard */
+  transition: color 0.4s ease 0s;
+}
+
+.nav__logo:hover {
+  color: purple;
+}
+
+.nav__links {
+  list-style: none;
+}
+
+.nav__links li {
+  display: inline-block;
+  padding: 0px 20px;
+}
+
+.nav__links li a {
+  transition: color 0.4s ease 0s;
+}
+
+.nav__links li a:hover {
+  /*color: #f0f0f0;*/
+  color: purple;
+}
+
+button {
+  padding: 9px 25px;
+  background-color: rgba(0, 136, 189, 1);
+  border: none;
+  border-radius: 40px;
+  cursor: pointer;
+  transition: background-color 0.4s ease 0s;
+}
+
+button:hover {
+  background-color: rgba(0, 136, 189, 0.8);
+}
+</style>
