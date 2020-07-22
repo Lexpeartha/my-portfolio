93e23121defd3217d9d338b92813dd67857c5aef
Lex <sevic.official@gmail.com>
2020-07-22 17:17:32 +0200

    Implementing navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <TheNav />
+  </div>
 </template>
 
 <script>
+import TheNav from "@/components/TheNavigation.vue";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    TheNav
+  }
 };
 </script>
 
--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <nav class="row">
+    <h4 class="column"><strong>PORTFOLIO</strong></h4>
+    <h5 class="column">About me</h5>
+    <h5 class="column">Projects</h5>
+    <h5 class="column">More</h5>
+  </nav>
 </template>
 
 <script>
