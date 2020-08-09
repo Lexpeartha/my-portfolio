a8b5b27f87fcad69026d835fd451c0eef9a0a4e0
Lex <sevic.official@gmail.com>
2020-08-09 19:06:27 +0200

    Improving footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,24 +1,45 @@
 <template>
-  <footer>
-    <a href="https://iradesign.io">Illustrations by IRA Design (Footer)</a>
+  <footer :style="breakpoint == `small` ? `display: flex;` : `display: flex;`">
+    <p>All rights reserved©</p>
+    <a href="https://iradesign.io">Illustrations done by IRA Design</a>
+    <div>
+      <img alt="Instragram" :src="require(`@/assets/logo/instagram.svg`)" />
+      <img alt="Github" :src="require(`@/assets/logo/github.svg`)" />
+    </div>
   </footer>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  name: "Footer"
+  name: "Footer",
+  computed: {
+    ...mapGetters(["breakpoint"])
+  }
 };
 </script>
 
 <style scoped>
 footer {
-  display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
   background-color: #201a1a;
   padding: 20px 10%;
+  color: whitesmoke;
   width: 100%;
 }
-footer a {
+footer > p {
+  opacity: 1;
+}
+footer > a {
   color: whitesmoke;
+}
+footer > div {
+  display: flex;
+}
+footer div > img {
+  height: 32px;
+  margin-left: 6px;
+  margin-right: 6px;
 }
 </style>
